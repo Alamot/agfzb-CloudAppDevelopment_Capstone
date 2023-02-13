e412ec001aefb645ff1cfd46d8acc39c4bddb5f3
alamot <antalamot@gmail.com>
2023-02-13 18:06:15 +0200

    Automatic commit Mon Feb 13 06:06:15 PM EET 2023

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,24 +4,26 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    carmake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()    
+    name = models.CharField(max_length=100)  
+    car_type = models.CharField(choices=(('Sedan','Sedan'),
+                                         ('SUV', 'SUV'),
+                                         ('WAGON', 'WAGON'),
+                                         ('COUPE', 'COUPE'),
+                                         ('SPORTS', 'SPORTS CAR'),),
+                                max_length=100)
+    year = models.DateField()
+    
+    def __str__(self):
+        return self.name
 
-
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
-
-# <HINT> Create a plain Python class `DealerReview` to hold review data
